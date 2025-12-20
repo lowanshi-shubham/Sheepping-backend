2193039521531074880081ad80cc373aef5f0131
shubham <shubhamlowanshi12345@gmail.com>
2025-12-20 13:43:22 +0530

    change

--- multer/multer.js
+++ multer/multer.js
@@ -2,7 +2,9 @@ import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "./cloudinary.js";
 
-const storage = new CloudinaryStorage({
+const storage = new CloudinaryStorage(
+  console.log("multer"),
+  {
   cloudinary,
   params: {
     folder: "products",
